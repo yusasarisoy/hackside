1102d4e2b77ad66b062ecb485d2118cd50540b67
Yuşa Sarısoy <yusasarisoy@icloud.com>
2021-04-26 20:28:18 +0300

    Initial Commit

---
+++ hackside/hacksideApp.swift
@@ -0,0 +1,17 @@
+//
+//  hacksideApp.swift
+//  hackside
+//
+//  Created by Yuşa Sarısoy on 26.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct hacksideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
